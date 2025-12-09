1fe1f5dacbad1609658170646ed34a51f6c06ff5
potato-124 <rahimovich3126@gmail.com>
2025-12-09 03:56:34 +0200

    Started work on pedro pathing
    Added in the drivetrain mecanum constants, and the drive encoder constants.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -3,17 +3,52 @@ package org.firstinspires.ftc.teamcode.pedroPathing;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.follower.FollowerConstants;
 import com.pedropathing.ftc.FollowerBuilder;
+import com.pedropathing.ftc.drivetrains.MecanumConstants;
+import com.pedropathing.ftc.localization.Encoder;
+import com.pedropathing.ftc.localization.constants.DriveEncoderConstants;
 import com.pedropathing.paths.PathConstraints;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Constants {
-    public static FollowerConstants followerConstants = new FollowerConstants();
+    public static DriveEncoderConstants localizerConstants = new DriveEncoderConstants()
+            .rightFrontMotorName("RFMotor")
+            .rightRearMotorName("RBMotor")
+            .leftRearMotorName("LBMotor")
+            .leftFrontMotorName("LFMotor")
+            .leftFrontEncoderDirection(Encoder.FORWARD)
+            .leftRearEncoderDirection(Encoder.FORWARD)
+            .rightFrontEncoderDirection(Encoder.REVERSE)
+            .rightRearEncoderDirection(Encoder.REVERSE)
+            .robotWidth(17.7165)
+            .robotLength(17.7165)
+            .forwardTicksToInches()
+            .strafeTicksToInches()
+            .turnTicksToInches()
 
+    public static MecanumConstants driveConstants = new MecanumConstants()
+            .maxPower(1)
+            .rightFrontMotorName("RFMotor")
+            .rightRearMotorName("RBMotor")
+            .leftRearMotorName("LBMotor")
+            .leftFrontMotorName("LFMotor")
+            .leftFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .leftRearMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .rightFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .rightRearMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .xVelocity(velocity)//forward
+            .yVelocity(velocity)//sideways to the left
+    public static FollowerConstants followerConstants = new FollowerConstants()
+            .mass()
+            .forwardZeroPowerAcceleration(deceleration)
+            .lateralZeroPowerAcceleration(deceleration)
     public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
 
     public static Follower createFollower(HardwareMap hardwareMap) {
         return new FollowerBuilder(followerConstants, hardwareMap)
+                .driveEncoderLocalizer(localizerConstants)
                 .pathConstraints(pathConstraints)
+                .mecanumDrivetrain(driveConstants)
                 .build();
     }
 }
