f7f6f0e0964fc9c34bdf6ea2a262b811dd25da42
Yourosan <yassenabufarees@gmail.com>
2026-02-16 10:21:23 +0200

    Finished constants tuning.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -13,22 +13,22 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 public class Constants {
     public static FollowerConstants followerConstants = new FollowerConstants()
             .mass(9)
-            .forwardZeroPowerAcceleration(-67)
-            .lateralZeroPowerAcceleration(-120);
+            .forwardZeroPowerAcceleration(-102.6645)
+            .lateralZeroPowerAcceleration(-213.72467);
 
 
     public static MecanumConstants driveConstants = new MecanumConstants()
             .maxPower(1)
-            .rightFrontMotorName("RB Motor")                 // NOTE:
-            .rightRearMotorName("RF Motor")                  // Motor names are intentionally cross-mapped.
-            .leftRearMotorName("LF Motor")                   // Physical Motor orientation is mirrored on this robot.
-            .leftFrontMotorName("LB Motor")                  // Changing these WILL break Pedro pathing.
+            .rightFrontMotorName("RF Motor")
+            .rightRearMotorName("RB Motor")
+            .leftRearMotorName("LB Motor")
+            .leftFrontMotorName("LF Motor")
             .leftFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
             .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
             .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
             .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD)
-            .xVelocity(65.55597842136162)
-            .yVelocity(82.5585);
+            .xVelocity(82.21)
+            .yVelocity(90.211875);
 
 
     public static DriveEncoderConstants localizerConstants = new DriveEncoderConstants()
@@ -44,7 +44,7 @@ public class Constants {
             .robotWidth(17.7165)
             .forwardTicksToInches(0.008226)
             .strafeTicksToInches(0.009358)
-            .turnTicksToInches(0.0007);
+            .turnTicksToInches(0.015722);
 
 
     public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
